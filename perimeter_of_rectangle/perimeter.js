faf322bcbf44fe2a004de173e6eedc67ff221573
SushantDahal <SushanDahal246@gmail.com>
2023-05-27 13:15:55 +0545

    perimter of Rectangle

---
+++ perimeter_of_rectangle/perimeter.js
@@ -0,0 +1,9 @@
+function perimeter(l,b){
+    return 2*(l+b)
+}
+const length = 10;
+const breadth  = 10;
+
+perimeter_of_Rectangle = perimeter(length ,breadth);
+
+console.log("Perimter of Rectangle is "+ perimeter_of_Rectangle);
